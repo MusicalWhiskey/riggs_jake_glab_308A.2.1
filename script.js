d0511e52599a4cec8a42c7a2b3c5c654ebb825e9
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-10-06 03:16:26 -0700

    test

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 console.log('GLAB 308A.2.1 test push test');
 
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat"
+    }
+    }
+    
