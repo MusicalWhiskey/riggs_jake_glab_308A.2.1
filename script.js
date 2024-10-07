741b4dc2391701b9809af35a07d2fa1bcdedaeb9
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-10-06 21:13:08 -0700

    testing dice role

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('GLAB 308A.2.1 test push test');
+console.log('Adventure Time!!! (GLAB 308A.2.1)');
 
 const adventurer = {
     name: "Robin",
@@ -10,14 +10,42 @@ const adventurer = {
     }
     }
 
-
-
     // Dot Notation
-    console.log(adventurer.inventory);
-    // Bracket Notation
-    console.log(adventurer["inventory"]); 
+    // console.log(adventurer.inventory);
+    // // Bracket Notation
+    // console.log(adventurer["inventory"]); 
 
+    //Alternative Log
+    // adventurer.inventory.forEach(item => console.log(item));
+
+    //Log Adventurer's Items (better)
     console.log(`${adventurer.name}'s Inventory:`);
     for (let i = 0; i < adventurer.inventory.length; i++) {
         console.log(`- ${adventurer.inventory[i]}`);
     }
+
+    //Adding Companion's Companion
+    adventurer.companion.companion = {
+        name: "Frank",
+        type: "Flea",
+        belongings: ["hat", "sunglasses"]
+     };
+    
+//Method for rolling dice
+
+ adventurer.roll = function(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+ };
+
+ class Character {
+    constructor(name) {
+       this.name = name;
+       this.health = 100;
+       this.inventory = [];
+    }
+    roll(mod = 0) {
+       const result = Math.floor(Math.random() * 20) + 1 + mod;
+       console.log(`${this.name} rolled a ${result}.`);
+    }
+ }
